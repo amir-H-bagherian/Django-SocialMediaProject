277bd5f46a7ad352a19559a2bdf2c65ff5d2e112
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-12 23:31:11 +0330

    DjangoSocialMediaProject: ModelAdmin objects added to admin.py file to customize admin panel for defined models

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Post, Comment
-# Register your models here.
 
 
-admin.site.register(Post)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at", "author", "content")
+    list_filter = ("is_available", )
+    search_fields = ("title", "author", "created_at")
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(is_available=True)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "body", "created_at")
+    list_filter = ("created_at", )
+    search_fields = ("post", "created_at")
+ 
+        
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
